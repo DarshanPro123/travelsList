8420f8bce870dfe482bd850cb24821385e5fc3b4
Darshanpro123 <darshanpro187@gmail.com>
2024-09-23 15:01:20 +0530

    Add form logics for submitig data in state

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
+
 export const Form = () => {
+  const [desc, setDesc] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault(); //it's not reload page after submiting form
+    console.log(desc); //print a data of desc in console.log
+    // after data get from input it's clear input in screen
+    setDesc("");
+  };
+
+  const handleChange = (e) => {
+    setDesc(e.target.value);
+  };
+
   return (
     <>
-      <div className="add-form">
+      <form className="add-form" onSubmit={handleSubmit}>
         <h3>What do you want 👼 for trip ? </h3>
-      </div>
+        <select>
+          {Array.from({ length: 15 }, (c, i) => i + 1).map((no) => (
+            <option value={no} key={no}>
+              {no}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="add Item.."
+          value={desc}
+          onChange={handleChange}
+        />
+        <button>Add</button>
+      </form>
     </>
   );
 };
--- src/Components/Pakgeing.jsx
+++ src/Components/Pakgeing.jsx
@@ -1,7 +1,9 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Book", quantity: 2, packed: true },
+  { id: 3, description: "Book", quantity: 2, packed: false },
+  { id: 4, description: "Charger", quantity: 1, packed: false },
+  { id: 5, description: "Laptops", quantity: 2, packed: true },
 ];
 
 const Pakgeing = () => {
@@ -12,7 +14,7 @@ const Pakgeing = () => {
         <ul>
           {initialItems.map((item) => {
             return (
-              <>
+              <div key={item.id}>
                 <li>
                   <span
                     //   onClick={handlPackege}
@@ -24,7 +26,7 @@ const Pakgeing = () => {
                   </span>
                   <button>❌</button>
                 </li>
-              </>
+              </div>
             );
           })}
         </ul>
